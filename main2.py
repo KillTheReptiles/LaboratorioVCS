a5c6fe51e62503b542eb0ae49dfa57e5e4b2b349
KillTheReptiles <duvan.diaz.2019@upb.edu.co>
2019-02-22 20:27:06 -0500

    Primera Modificacion

---
+++ main2.py
@@ -0,0 +1,24 @@
+import math
+print("Comienzo")
+print()
+n_a = input("ingrese un numero: ")
+n_c = input("ingrese un numero: ")
+
+producto = int(n_a) * int(n_c)
+doble_na = 2*int(n_a)
+print (producto)
+print (doble_na)
+
+n_b = input("ingrese un numero: ")
+
+potencia_pow = pow(int(n_b), 2)
+print(potencia_pow)
+
+
+n_d=input("Ingrese numero: ")
+raiz=math.sqrt(int(n_d))
+print (raiz)
+
+print("Se ha calculado bien!")
+print()
+print("Final")
