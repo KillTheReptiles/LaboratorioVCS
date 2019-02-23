4a29782cc0aaa501549b8d945db06442415f0900
KillTheReptiles <duvan.diaz.2019@upb.edu.co>
2019-02-22 20:26:14 -0500

    Primer Commit

---
+++ Main.py
@@ -0,0 +1,15 @@
+# Escribe tu código aquí :-)
+print("Comienzo")
+print()
+n_a = input("ingrese un numero ")
+n_c = input("ingrese un numero ")
+
+P = int(n_a) * int(n_c)
+D_na = 2*int(n_a)
+print (P)
+print (D_na)
+
+
+
+print()
+print("Final")
